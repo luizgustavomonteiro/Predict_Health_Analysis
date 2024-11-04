c514831c7272bea5116014c62e5c84564c79aae0
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2024-11-04 16:06:39 +0530

    View-Trends-Maps

--- predictHealth/myapp/urls.py
+++ predictHealth/myapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name ="home"),
+    path('maps', views.maps_views, name = "maps"),
+    path('trends', views.trends_views, name = "trends"),
 ]
+
+
--- predictHealth/myapp/views.py
+++ predictHealth/myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+#from .models import Regions, Diseases, Notifications
 
 # Create your views here.
+# This is the controler, the brain behind the app
 
 def home(request):
     return render(request, 'home.html', {})
+
+def trends_views(request):
+    return render(request, 'trends.html', {})
+
+def maps_views(request):
+    return render(request, 'maps.html', {})
